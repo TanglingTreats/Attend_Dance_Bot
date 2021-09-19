61a8932e133a550dcb109318cdaa2de99cefe410
Edwin <edwintang10@gmail.com>
2021-09-19 19:56:34 +0800

    Add handler for help command

--- attend_dance_bot.py
+++ attend_dance_bot.py
@@ -13,12 +13,12 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, 
             text="I\'m here to help manage your attendance")
 
-def help(update, context):
+def help_command(update, context):
     message="Thanks for using the bot!\n\
         \\create_attendance to start your attendance list!"
 
     context.bot.send_message(chat_id=update.effective_chat.id,
-            text="Help guide")
+            text=message)
 
 def create_attendance(update,context):
     context.bot.send_message(chat_id=update.effective_chat.id,
@@ -38,11 +38,13 @@ def main():
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help_command)
     create_attendance_handler = CommandHandler('create_attendance', create_attendance)
     edit_attendance_handler = CommandHandler('edit_attendance', edit_attendance)
     end_attendance_handler = CommandHandler('end_attendance', end_attendance)
 
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
     dispatcher.add_handler(create_attendance_handler)
     dispatcher.add_handler(edit_attendance_handler)
     dispatcher.add_handler(end_attendance_handler)
