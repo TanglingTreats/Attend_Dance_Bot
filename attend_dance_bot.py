5c6448fead8f4f698ea6c482470c89ed63199ef9
Edwin Tang <edwintang10@gmail.com>
2021-09-26 16:11:16 +0000

    Fix create attendance conversation flow

--- attend_dance_bot.py
+++ attend_dance_bot.py
@@ -18,6 +18,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 ENTER_DATE, ENTER_EVENT, ENTER_OPTIONS = range(3)
 
+END = ConversationHandler.END
+
 def start(update: Update, context: CallbackContext):
     message = "I\'m here to help manage your attendance"
     context.bot.send_message(chat_id=update.effective_chat.id, 
@@ -38,22 +40,34 @@ def create_attendance(update: Update, context: CallbackContext):
 
 def retrieve_event_date(update: Update, context: CallbackContext):
     text = update.message.text
-    reply = f'{text}'
+    reply = f'Creating an event on {text}'
     update.message.reply_text(reply)
+
+    message = "Next, what's the event about?"
+    context.bot.send_message(chat_id=update.effective_chat.id,
+            text=message)
     
     return ENTER_EVENT
 
 def retrieve_event_detail(update: Update, context: CallbackContext):
     text = update.message.text
-    reply = f'{text}'
+    reply = f'Event is about: {text}'
     update.message.reply_text(reply)
+    
+    message = "Next, what's are your options for this attendance list?"
+    context.bot.send_message(chat_id=update.effective_chat.id,
+            text=message)
 
     return ENTER_OPTIONS
 
 def retrieve_event_options(update: Update, context: CallbackContext):
     text = update.message.text
-    reply = f'{text}'
+    reply = f'\'{text}\' has been entered'
     update.message.reply_text(reply)
+
+    message = "Continue typing your options, if not, type \"Done\""
+    context.bot.send_message(chat_id=update.effective_chat.id,
+            text=message)
 
     return ENTER_OPTIONS
 
@@ -69,6 +83,7 @@ def end_attendance(update: Update, context: CallbackContext):
 def done(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
             text="You are done creating the attendance")
+    return END
 
 def regular_choice(update: Update, context: CallbackContext) -> int:
     """Ask the user for info about the selected predefined choice."""
@@ -116,12 +131,14 @@ def main():
     )
 
 
+    # Command Handlers
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(conv_handler)
-    #dispatcher.add_handler(create_attendance_handler)
     dispatcher.add_handler(edit_attendance_handler)
     dispatcher.add_handler(end_attendance_handler)
+
+    # Conversation Handlers
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
 
